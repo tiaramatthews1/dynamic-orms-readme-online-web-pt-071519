dba2c9d01de3bf2e8167fc522b3b30fe023ca3fb
Tiara Matthews <tiaramatthews96@yahoo.com>
2019-09-28 22:58:34 +0000

    Automatically backed up by Learn

--- lib/song.rb
+++ lib/song.rb
@@ -1,3 +1,24 @@
 require_relative "../config/environment.rb"
 require 'active_support/inflector'
 
+class Song
+  attr_accessor :name, :album
+  attr_reader :id
+ 
+  def initialize(id=nil, name, album)
+    @id = id
+    @name = name
+    @album = album
+  end
+ 
+  def self.create_table
+    sql =  <<-SQL
+      CREATE TABLE IF NOT EXISTS songs (
+        id INTEGER PRIMARY KEY,
+        name TEXT,
+        album TEXT
+        )
+    SQL
+    DB[:conn].execute(sql)
+  end
+end
